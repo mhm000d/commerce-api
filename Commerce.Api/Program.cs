ebaa514eeb098d4291deec9ce10188d1fc7e48f6
Mahmoud <mahmoudjamal01m@gmail.com>
2026-05-17 08:34:34 +0300

    feat: configure SMTP settings and enhance application startup

--- Commerce.Api/Program.cs
+++ Commerce.Api/Program.cs
@@ -1,40 +1,30 @@
 using Commerce.Api;
+using Commerce.Api.Startup;
 using Commerce.Application;
-using Commerce.Application.Database;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddApiDocumentation();
+builder.Services.AddConfiguredCors(builder.Configuration);
+builder.Services.AddApiHealthChecks();
+builder.Services.AddConfiguredRateLimiting(builder.Configuration, builder.Environment);
 
 builder.Services.AddDatabase(builder.Configuration);
-builder.Services.AddApplication(builder.Configuration);
+builder.Services.AddApplication(builder.Configuration, builder.Environment);
 builder.Services.AddAuthServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.UseApiDocumentation();
 app.UseMiddleware<GlobalExceptionMiddleware>();
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+app.UseApiSecurity();
+app.UseRequestBodyBuffering();
+app.UseHangfireDashboardAndJobs();
+
 app.MapControllers();
+app.MapApiHealthChecks();
 
-if (!app.Environment.IsEnvironment("Testing"))
-{
-    using var scope = app.Services.CreateScope();
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-    await dbContext.Database.MigrateAsync();
-    await DbSeeder.SeedAsync(dbContext);
-}
+await app.MigrateAndSeedDatabaseAsync();
 
 app.Run();
